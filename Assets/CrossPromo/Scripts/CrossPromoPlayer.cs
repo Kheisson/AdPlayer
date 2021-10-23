70fb0495dc28ea37b8c755ecaf64c083f644f533
Kson <igor.kheison@gmail.com>
2021-10-23 13:41:16 +0300

    Added enable/disable logic

--- Assets/CrossPromo/Scripts/CrossPromoPlayer.cs
+++ Assets/CrossPromo/Scripts/CrossPromoPlayer.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 namespace CrossPromo.Scripts
@@ -13,6 +15,8 @@ namespace CrossPromo.Scripts
         [SerializeField] private int playerId;
 
         [SerializeField] private VideoPlayer videoPlayer;
+        
+        [SerializeField] private Button adButton;
 
         private string serverEndpoint = "https://run.mocky.io/v3/81fab340-9550-4ab4-8859-836b01ee48ff";
 
@@ -20,17 +24,32 @@ namespace CrossPromo.Scripts
 
         private int _currentVideoIndex = 0;
 
+        private bool _isRunning;
+
         private Coroutine _videoPlaylistCoroutine;
 
         private Dictionary<string, bool> _trackingUrls = new Dictionary<string, bool>();
 
+
+
         private void Awake()
         {
-            if (GameObject.FindObjectOfType<EventSystem>() is null)
+            _isRunning = true;
+            
+            //Required components in case canvas is missing defaulted event system GameObject
+            if (FindObjectOfType<EventSystem>() is null)
             {
                 gameObject.AddComponent<EventSystem>();
                 gameObject.AddComponent<StandaloneInputModule>();
             }
+            DontDestroyOnLoad(gameObject);     //In case of instant load, GameObject is kept
+
+            adButton.onClick.AddListener(VideoClicked);
+        }
+
+        private void OnDestroy()
+        {
+            adButton.onClick.RemoveListener(VideoClicked);
         }
 
         private IEnumerator Start()
@@ -42,6 +61,7 @@ namespace CrossPromo.Scripts
             _metaDataList = req.Data.results;
 
             _videoPlaylistCoroutine = StartCoroutine(RunVideo());
+
         }
 
         private IEnumerator RunVideo()
@@ -59,20 +79,28 @@ namespace CrossPromo.Scripts
 
             while (videoPlayer.isPlaying)
             {
-                Debug.Log($"Playing video '{_currentVideoIndex}'");
                 yield return null;
             }
 
             if (_currentVideoIndex < _metaDataList.Count && videoPlayer.isPaused)
             {
                 _currentVideoIndex++;
-                _currentVideoIndex = Mathf.Clamp(_currentVideoIndex, 0, _metaDataList.Count - 1);
+                _currentVideoIndex = Mathf.Clamp(_currentVideoIndex, 0, _metaDataList.Count);
+                
+                //Playlist finished playing all videos
+                if (_currentVideoIndex == _metaDataList.Count)
+                {
+                    gameObject.SetActive(false);
+                    yield break;
+                }
+
                 CoroutineCheck();
             }
         }
-
+        
         private void CoroutineCheck()
         {
+            //Coroutine stopper in case of interruption, e.g. - Pause is called.
             if (_videoPlaylistCoroutine != null)
             {
                 StopCoroutine(_videoPlaylistCoroutine);
@@ -81,6 +109,37 @@ namespace CrossPromo.Scripts
             _videoPlaylistCoroutine = StartCoroutine(RunVideo());
         }
 
+        private void VideoClicked()
+        {
+            //Attached to renderer, will be called when Ad is clicked
+            var trackingUrl = _metaDataList[_currentVideoIndex].tracking_url
+                .Replace("[PLAYER_ID]", playerId.ToString());
+            
+            if (!_trackingUrls.ContainsKey(trackingUrl))
+            {
+                _trackingUrls.Add(trackingUrl, true);
+                var post = new PostCrossPromoMetaDataRequest(trackingUrl, string.Empty);
+                StartCoroutine(post.SendRequest());
+            }
+            
+            Application.OpenURL(_metaDataList[_currentVideoIndex].click_url);
+        }
+
+        private void OnEnable()
+        {
+            if (_isRunning) return;
+            _currentVideoIndex = 0;
+            _videoPlaylistCoroutine = StartCoroutine(RunVideo());
+            videoPlayer.Play();
+        }
+
+        private void OnDisable()
+        {
+            _isRunning = false;
+            _videoPlaylistCoroutine = null;
+        }
+
+        #region Player Controls
         public void Next()
         {
             _currentVideoIndex++;
@@ -124,21 +183,7 @@ namespace CrossPromo.Scripts
                 return;
             CoroutineCheck();
         }
-
-
-        public void GetCurrentVideo()
-        {
-            Pause();
-            var trackingUrl = _metaDataList[_currentVideoIndex].tracking_url
-                .Replace("[PLAYER_ID]", playerId.ToString());
-            if (!_trackingUrls.ContainsKey(trackingUrl))
-            {
-                _trackingUrls.Add(trackingUrl, true);
-                var post = new PostCrossPromoMetaDataRequest(trackingUrl, string.Empty);
-                StartCoroutine(post.SendRequest());
-            }
-            
-            Application.OpenURL(_metaDataList[_currentVideoIndex].click_url);
-        }
+        
+        #endregion
     }
 }
